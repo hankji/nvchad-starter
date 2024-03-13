d8646ae0f85cb091a845189262437b9c9424b778
jihaiming <jihaiming@ushareit.com>
2024-03-13 12:23:29 +0800

    fix: codeium load

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -83,6 +83,7 @@ return {
 	},
 	{
 		"Exafunction/codeium.vim",
+		event = "BufEnter",
 		config = function()
 			-- Change '<C-g>' here to any keycode you like.
 			vim.keymap.set("i", "<C-g>", function()
