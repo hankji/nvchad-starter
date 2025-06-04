415862b79702c3f8b8fccba15e970a3560df6767
jihaiming <jihaiming@ushareit.com>
2025-06-04 15:11:14 +0800

    change vim.diagnostic.config

--- lua/configs/lsp-config.lua
+++ lua/configs/lsp-config.lua
@@ -42,6 +42,25 @@ lspconfig.gopls.setup {
 
 -- Disable virtual_text since it's redundant due to lsp_lines.
 vim.diagnostic.config {
-  virtual_text = false,
-  virtual_lines = true,
+  -- virtual_text = false,
+  virtual_lines = false,
+  virtual_text = {
+    prefix = "●", -- 可以是图标、"●"、"■"等
+    spacing = 4, -- 与文本的间距
+    severity = vim.diagnostic.severity.ERROR, -- 只显示 ERROR 级别
+    format = function(diagnostic)
+      return diagnostic.code or diagnostic.message
+    end,
+  },
+  underline = {
+    severity = { min = vim.diagnostic.severity.ERROR },
+  },
+  float = {
+    show_header = true,
+    source = "always",
+    border = "rounded",
+    format = function(d)
+      return string.format("%s [%s]", d.message, d.source)
+    end,
+  },
 }
