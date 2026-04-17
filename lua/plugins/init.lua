49706bce738f297d12b05015fa4021cadd0f0fd9
jihaiming <jihaiming@ushareit.com>
2026-04-17 11:03:50 +0800

    update for textobjects

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -82,16 +82,16 @@ return {
       }
     end,
   },
-  {
-    "ThePrimeagen/refactoring.nvim",
-    dependencies = {
-      { "nvim-lua/plenary.nvim" },
-      { "nvim-treesitter/nvim-treesitter" },
-    },
-    config = function()
-      require("refactoring").setup()
-    end,
-  },
+  -- {
+  --   "ThePrimeagen/refactoring.nvim",
+  --   dependencies = {
+  --     { "nvim-lua/plenary.nvim" },
+  --     { "nvim-treesitter/nvim-treesitter" },
+  --   },
+  --   config = function()
+  --     require("refactoring").setup()
+  --   end,
+  -- },
   {
     "Exafunction/codeium.vim",
     event = "BufEnter",
